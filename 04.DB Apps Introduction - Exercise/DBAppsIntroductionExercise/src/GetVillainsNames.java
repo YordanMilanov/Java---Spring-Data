05bc53cc7cf583219addf1e3d56cb0fdfe8c53ba
Yordan Milanov <dancho.galinov@gmail.com>
2022-10-31 09:21:43 +0200

    task 1 completed

--- 04.DB Apps Introduction - Exercise/DBAppsIntroductionExercise/src/GetVillainsNames.java
+++ 04.DB Apps Introduction - Exercise/DBAppsIntroductionExercise/src/GetVillainsNames.java
@@ -1,11 +1,31 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 public class GetVillainsNames {
+
+    private static final String COLUMN_LABEL_NAME = "name";
+    private static final String COLUMN_LABEL_MINIONS_COUNT = "minions_count";
+    private static final String PRINT_FORMAT = "%s %d";
+    private static final String GET_VILLAINS_NAMES ="select v.name, COUNT(distinct mv.minion_id) as minions_count from villains as v\n" +
+            " join minions_villains mv on v.id = mv.villain_id" +
+            " GROUP BY mv.villain_id" +
+            " having minions_count > ?" +
+            " order by minions_count;";
     public static void main(String[] args) throws SQLException {
        final Connection connection = Utils.getSQLConnection();
 
+       final PreparedStatement statement = connection.prepareStatement(GET_VILLAINS_NAMES);
+
+       statement.setInt(1, 15);
+
+       final ResultSet resultSet = statement.executeQuery();
+
+       while (resultSet.next()) {
+           final String villainName = resultSet.getString(COLUMN_LABEL_NAME);
+           final int minionsCount = resultSet.getInt(COLUMN_LABEL_MINIONS_COUNT);
+        System.out.printf(PRINT_FORMAT, villainName, minionsCount);
+       }
+
+       connection.close();
     }
 }
