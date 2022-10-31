32b461b7e56f6a6ab1ec385ea243bf33964a85f5
Yordan Milanov <dancho.galinov@gmail.com>
2022-10-31 08:54:19 +0200

    jdbc apps introduction

--- 04.DB Apps Introduction - Exercise/DBAppsIntroductionExercise/src/GetVillainsNames.java
+++ 04.DB Apps Introduction - Exercise/DBAppsIntroductionExercise/src/GetVillainsNames.java
@@ -5,10 +5,7 @@ import java.util.Properties;
 
 public class GetVillainsNames {
     public static void main(String[] args) throws SQLException {
-        Properties properties = new Properties();
-        properties.setProperty("user", "root");
-        properties.setProperty("password", "");
+       final Connection connection = Utils.getSQLConnection();
 
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/minions_db");
     }
 }
