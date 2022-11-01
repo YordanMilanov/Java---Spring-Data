8e35d9640c85c00126ebe5aaaf7752bc24b19090
Yordan Milanov <dancho.galinov@gmail.com>
2022-11-01 18:25:27 +0200

    task

--- 05.ORM Fundamentals - Lab/CustomORM/src/main/java/orm/EntityManager.java
+++ 05.ORM Fundamentals - Lab/CustomORM/src/main/java/orm/EntityManager.java
@@ -6,7 +6,9 @@ import annotations.Id;
 
 import java.lang.reflect.Field;
 import java.sql.Connection;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class EntityManager<E> implements DBContext<E> {
@@ -32,22 +34,41 @@ public class EntityManager<E> implements DBContext<E> {
         return false;
     }
 
-    private boolean doInsert(E entity, Field idColumn) {
+    private boolean doInsert(E entity, Field idColumn) throws IllegalAccessException {
         String tableName = getTableName(entity.getClass());
-        getColumnsWithoutId(entity.getClass());
+        String tableFields = getColumnsWithoutId(entity.getClass());
+        String tableValues = getColumnValuesWithoutId(entity);
 
-        String insertQuery = String.format("INSERT INTO %s (%s) VALUES ???", tableName);
+        String insertQuery = String.format("INSERT INTO %s (%s) VALUES (%s)", tableName, tableFields, tableValues);
 
         return false;
     }
 
-    private void getColumnsWithoutId(Class<?> aClass) {
-        Arrays.stream(aClass.getDeclaredFields())
+    private String getColumnValuesWithoutId(E entity) throws IllegalAccessException {
+        Class<?>  aClass = entity.getClass();
+        List<Field> fields = Arrays.stream(aClass.getDeclaredFields())
+                .filter(f -> !f.isAnnotationPresent(Id.class))
+                .filter(f -> !f.isAnnotationPresent(Column.class))
+                .collect(Collectors.toList());
+
+        List<String> values = new ArrayList<>();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            Object o = field.get(entity);
+            values.add(o.toString());
+
+        }
+
+       return String.join(", ", values);
+    }
+
+    private String getColumnsWithoutId(Class<?> aClass) {
+        return Arrays.stream(aClass.getDeclaredFields())
                 .filter(f -> !f.isAnnotationPresent(Id.class))
                 .filter(f -> !f.isAnnotationPresent(Column.class))
                 .map(f -> f.getAnnotationsByType(Column.class))
                 .map (a -> a[0].name())
-                .collect(Collectors.toList());
+                .collect(Collectors.joining(","));
     }
 
     private String getTableName(Class<?> aClass) {
