4a6e737a83747ecad70bb01a81ec4cc82ec6da1f
YordanMilanov <dancho.galinov@gmail.com>
2022-11-03 22:35:49 +0200

    task5 100/100

--- 08.Hibernate Intro - Exercise/src/main/java/Main_05EmployeesFromDepartment.java
+++ 08.Hibernate Intro - Exercise/src/main/java/Main_05EmployeesFromDepartment.java
@@ -1,2 +1,26 @@
+import entities.Employee;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.List;
+
 public class Main_05EmployeesFromDepartment {
+    public static void main(String[] args) {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("PU_Name");
+        EntityManager entityManager = factory.createEntityManager();
+        entityManager.getTransaction().begin();
+
+        String department = "Research and Development";
+
+        entityManager
+                .createQuery("SELECT e FROM Employee e WHERE e.department.name = :departmentName ORDER BY e.salary ASC, e.id ASC", Employee.class)
+                .setParameter("departmentName", department)
+                .getResultStream().forEach(e -> {
+            String output = String.format("%s %S from %s - %.2f$", e.getFirstName(), e.getLastName(), department, e.getSalary());
+                    System.out.println(output);
+                });
+
+        entityManager.getTransaction().commit();
+    }
 }
