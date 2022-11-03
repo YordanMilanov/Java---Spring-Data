4ee8a13fe4239e3e982b9756ce0dc30281118b14
YordanMilanov <dancho.galinov@gmail.com>
2022-11-03 20:24:59 +0200

    task 3

--- 08.Hibernate Intro - Exercise/src/main/java/Main_03CheckEmployeeExists.java
+++ 08.Hibernate Intro - Exercise/src/main/java/Main_03CheckEmployeeExists.java
@@ -1,5 +1,30 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.Scanner;
+
 public class Main_03CheckEmployeeExists {
     public static void main(String[] args) {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("PU_Name");
+        EntityManager entityManager = factory.createEntityManager();
+        entityManager.getTransaction().begin();
 
+        Scanner scanner = new Scanner(System.in);
+        String[] searchFor = scanner.nextLine().split(" ");
+        String firstName = searchFor[0];
+        String lastName = searchFor[1];
+
+        Long employeeCount = entityManager.createQuery("SELECT COUNT(e) FROM Employee e WHERE e.firstName = :first_name AND e.lastName = :last_name", Long.class)
+                .setParameter("first_name", firstName)
+                .setParameter("last_name", lastName)
+                .getSingleResult();
+
+        if (employeeCount > 0) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+        entityManager.getTransaction().commit();
     }
 }
