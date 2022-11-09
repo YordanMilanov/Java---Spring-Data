a4aaa7bc06ca7f5f587102e1ed72a91007196d59
YordanMilanov <dancho.galinov@gmail.com>
2022-11-09 12:19:49 +0200

    changes

--- 08.Hibernate Intro - Exercise/src/main/java/Main_10IncreaseSalaries.java
+++ 08.Hibernate Intro - Exercise/src/main/java/Main_10IncreaseSalaries.java
@@ -1,2 +1,41 @@
-package entities;public class Main_10IncreaseSalaries {
+import entities.Employee;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main_10IncreaseSalaries {
+    public static void main(String[] args) {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("PU_Name");
+        EntityManager entityManager = factory.createEntityManager();
+        entityManager.getTransaction().begin();
+
+        List<String>departmentNames = new ArrayList<>();
+        departmentNames.add("Engineering");
+        departmentNames.add("Tool Design");
+        departmentNames.add("Marketing");
+        departmentNames.add("Information Services");
+
+        //update salaries
+       entityManager
+                .createQuery(
+                        "UPDATE Employee AS e " +
+                                "SET e.salary = e.salary * 1.12 " +
+                                "WHERE e.department.name IN('Engineering', 'Tool Design', 'Marketing', 'Information Services')").executeUpdate();
+
+        entityManager
+                .createQuery(
+                        "SELECT e FROM Employee e " +
+                                "WHERE e.department.name IN('Engineering', 'Tool Design', 'Marketing', 'Information Services')", Employee.class)
+                .getResultList()
+                .forEach(e -> System.out.printf("%s %s ($%.2f)%n",e.getFirstName(), e.getLastName(), e.getSalary()));
+
+
+
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
 }
