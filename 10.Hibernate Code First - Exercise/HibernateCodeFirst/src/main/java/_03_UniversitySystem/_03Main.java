dbc1602dfa471ee3c8e78438ab846e733f4197b7
YordanMilanov <dancho.galinov@gmail.com>
2022-11-11 22:21:15 +0200

    update

--- 10.Hibernate Code First - Exercise/HibernateCodeFirst/src/main/java/_03_UniversitySystem/_03Main.java
+++ 10.Hibernate Code First - Exercise/HibernateCodeFirst/src/main/java/_03_UniversitySystem/_03Main.java
@@ -1,4 +1,18 @@
 package _03_UniversitySystem;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class _03Main {
+    public static void main(String[] args) {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("code");
+
+        EntityManager entityManager = factory.createEntityManager();
+
+        entityManager.getTransaction().begin();
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
 }
