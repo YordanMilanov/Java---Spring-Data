2d1e2e4a740ed4667665caf2f46cda7bf454767e
YordanMilanov <dancho.galinov@gmail.com>
2022-11-27 23:11:21 +0200

    mapping exercise update

---
+++ 16.Spring Data Auto Mapping Objects - Exercise/GameStore/src/main/java/bg/softuni/gamestore/domain/entities/User.java
@@ -0,0 +1,80 @@
+package bg.softuni.gamestore.domain.entities;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity{
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, name = "full_name")
+    private String fullName;
+
+    @ManyToMany
+    private Set<Game> games;
+
+    @OneToMany
+    private Set<Order> orders;
+
+    private boolean isAdmin;
+
+    public User() {
+        this.games = new HashSet<>();
+        this.orders = new HashSet<>();
+    }
+
+    public User(String email, String password, String fullName) {
+        this(); // <- this calls the upper constructor
+        this.email = email;
+        this.password = password;
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Set<Game> getGames() {
+        return games;
+    }
+
+    public void setGames(Set<Game> games) {
+        this.games = games;
+    }
+
+    public boolean getIsAdmin() {
+        return isAdmin;
+    }
+
+    public void setIsAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+}
---
+++ 16.Spring Data Auto Mapping Objects - Exercise/GameStore/src/main/java/bg/softuni/gamestore/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package bg.softuni.gamestore.repositories;
+
+import bg.softuni.gamestore.domain.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
+
