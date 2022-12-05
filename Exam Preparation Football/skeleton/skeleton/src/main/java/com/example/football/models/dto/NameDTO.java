05727f1ab3b24893a39ab9629158eeb07257d7bb
YordanMilanov <dancho.galinov@gmail.com>
2022-12-05 14:41:35 +0200

    fix

--- Exam Preparation Football/skeleton/skeleton/src/main/java/com/example/football/models/dto/NameDTO.java
+++ Exam Preparation Football/skeleton/skeleton/src/main/java/com/example/football/models/dto/NameDTO.java
@@ -4,10 +4,12 @@ import lombok.Getter;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 @Getter
 public class NameDTO {
 
+    @XmlElement
     private String name;
 }
